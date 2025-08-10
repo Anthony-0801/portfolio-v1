3844b7622d7ba84d500fd23170c80ad9fc50f7e7
Anthony <zyxanthony15@gmail.com>
2025-08-10 22:26:51 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <h1>Welcome to Vite + React</h1>
+      <p>
+        This is my starting point in order to regain my will in programming. 
+      </p>
+      <Button>
+        Hello
+      </Button>
+    </>
+  )
+}
+
+export default App
